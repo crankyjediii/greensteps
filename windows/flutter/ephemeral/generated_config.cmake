f1e28c3e4940a56a1977dde0f13d75223fe4f019
Jimmy <jimmymullen2009@gmail.com>
2025-05-15 11:11:34 -0500

    Deploy Flutter web app to gh-pages

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\jm27053.LISLE202\\Desktop\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\jm27053.LISLE202\\Desktop\\GreenSteps\\App\\lib\\greensteps" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\jm27053.LISLE202\\Desktop\\Flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\jm27053.LISLE202\\Desktop\\GreenSteps\\App\\lib\\greensteps"
+  "FLUTTER_ROOT=C:\\Users\\jm27053.LISLE202\\Desktop\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\jm27053.LISLE202\\Desktop\\GreenSteps\\App\\lib\\greensteps\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\jm27053.LISLE202\\Desktop\\GreenSteps\\App\\lib\\greensteps"
+  "FLUTTER_TARGET=C:\\Users\\jm27053.LISLE202\\Desktop\\GreenSteps\\App\\lib\\greensteps\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\jm27053.LISLE202\\Desktop\\GreenSteps\\App\\lib\\greensteps\\.dart_tool\\package_config.json"
+)
